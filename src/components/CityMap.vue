803e153e22818945f18968f89fad05551adf096a
number304 <jmpmateo3042013@gmail.com>
2021-07-05 23:08:49 -0500

    geoman added

--- src/components/CityMap.vue
+++ src/components/CityMap.vue
@@ -9,6 +9,9 @@ import Vue from 'vue';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+
 export default Vue.extend({
   name: 'CityMap',
   props: {
@@ -33,6 +36,24 @@ export default Vue.extend({
         attribution:
           '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
       }).addTo(this.map);
+
+      // L.control.scale().addTo(this.map);
+
+      this.map.pm.addControls({
+        position: 'topleft',
+        drawMarker: false,
+        drawCircleMarker: false,
+        drawPolyline: false, // draw streight line
+        drawRectangle: false,
+        drawPolygon: true, // draw new shapes
+        drawCircle: false,
+        editMode: true, // edit shapes
+        dragMode: false,
+        cutPolygon: false,
+        removalMode: false, // erase shapes
+        pinningOption: false,
+        snappingOption: false,
+      });
     },
   },
 });
