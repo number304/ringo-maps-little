32cd4eecf8709a6b73a1cd5c8e653db9d063273e
number304 <jmpmateo3042013@gmail.com>
2021-08-11 14:38:58 -0500

    disabled geoman rotation button

--- src/components/CityMap.vue
+++ src/components/CityMap.vue
@@ -206,7 +206,8 @@ export default Vue.extend({
         cutPolygon: false,
         removalMode: false, // erase shapes
         pinningOption: false,
-        snappingOption: false
+        snappingOption: false,
+        rotateMode: false
       });
 
       // Creates an event listener that returns on console the
